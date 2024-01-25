9a64a14f98ca30896930fd8d66e0ddc516d74af7
nasir1196 <nasiruddin.cf@gmail.com>
2024-01-25 14:29:28 +0600

    initiate authentication

--- app/auth/register/RegisterButtonPage.tsx
+++ app/auth/register/RegisterButtonPage.tsx
@@ -1,29 +1,11 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 
-interface RegisterButtonProps {
-  children: React.ReactNode;
-  mode?: "modal" | "redirect";
-  asChild?: boolean;
-}
-
-const RegisterButtonPage = ({
-  children,
-  mode,
-  asChild,
-}: RegisterButtonProps) => {
-  const router = useRouter();
-  const onClick = () => {
-    router.push("/auth/login");
-  };
-
-  if (mode === "modal") {
-    return <span>TODO: Implement Modal</span>;
-  }
+const RegisterButtonPage = () => {
+ 
   return (
-    <span onClick={onClick} className="cursor-pointer">
-      {children}
+    <span>
+     
     </span>
   );
 };
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,17 +1,10 @@
-import { Button } from "@/components/ui/button";
-import RegisterButtonPage from "./RegisterButtonPage";
+
 
 const RegisterPage = () => {
   return (
     <div className="flex flex-col items-center h-full justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div>Register page</div>
-      <div className="space-y-6 text-center">
-        <RegisterButtonPage>
-          <Button variant="buttonGreen" size="lg">
-            Sign In
-          </Button>
-        </RegisterButtonPage>
-      </div>
+      
+      
     </div>
   );
 };
